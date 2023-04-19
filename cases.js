56c71e87b539fb2733999cdd3f6bb76017f33e7d
Riley <woodruff.l.riley@gmail.com>
2023-04-18 20:56:19 -0700

    Working on question words page

--- cases.js
+++ cases.js
@@ -2,3 +2,19 @@ $(function () {
   $("#header").load("header.html");
   $("#footer").load("footer.html");
 });
+
+// Define the question words and their corresponding answers
+var questions = {
+  "Что-Nom": "Что",
+  "Что-Acc": "Что",
+  "Что-Gen": "Чего",
+  "Что-Prep": "Чём",
+  "Что-Dat": "Чему",
+  "Что-Ins": "Чем",
+  "Кто-Nom": "Что",
+  "Кто-Acc": "Кого",
+  "Кто-Gen": "Кого",
+  "Кто-Prep": "Ком",
+  "Кто-Dat": "Кому",
+  "Кто-Ins": "Кем",
+};
